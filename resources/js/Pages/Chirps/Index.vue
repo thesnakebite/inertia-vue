e347e47cad8fcd9b7bab707db640f905671fde25
Álvaro <alvaro@thesnakebite.es>
2023-10-28 08:33:39 +0200

    Creating the chirps route

---
+++ resources/js/Pages/Chirps/Index.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Chirps Index</h1>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,10 @@ Route::get('/', function () {
     ]);
 });
 
+Route::get('/chirps', function () {
+    return Inertia::render('Chirps/Index');
+});
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
